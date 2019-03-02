fa2a09f41b835245ff4203b282c30ad1d7610346
Owl <owl@OwldeMacBook-Pro.local>
2019-03-02 12:01:03 +0800

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Owl on 2019/3/2.
+//  Copyright © 2019 Owl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
