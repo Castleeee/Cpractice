ecbe6d62b907dd2d6b37aebd05212ee853bc55c0
“owl <576359186@qq.com>
2019-03-02 12:24:42 +0800

    test1

---
+++ practice/dewfgsg.cpp
@@ -0,0 +1,16 @@
+/*include here*/
+#include "iostream"
+/*define here*/
+
+using namespace std;
+/*function&class*/
+
+//entry
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
+/*
+created by a_little_rubbish
+have a nice day : )
+*/
